9ac5cd1f429a0d96491181e16347cb074a2506aa
Isabelle-123 <isacas031@gmail.com>
2020-08-26 17:32:06 +0200

    added 2 links

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+A fullstacvk app to keep track on contacts
+
 //npm run dev to run client (5000) and server (3000)
 //npm run server to run server (3000)
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className='my-1'>This is an app to keep track on your contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  )
+}
+
+export default About
