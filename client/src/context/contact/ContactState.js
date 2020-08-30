b69a5a918f48d2a523431b6db47d12719a9036a0
Isabelle-123 <isacas031@gmail.com>
2020-08-30 12:19:10 +0200

    added contactcontext, types

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,42 @@
 // import uuid from 'uuid'
 // import ContactContext from './contactContext'
 // import contactReducer from './contactReducer'
+
+// import {
+//   GET_CONTACTS,
+//   ADD_CONTACT,
+//   DELETE_CONTACT,
+//   SET_CURRENT,
+//   CLEAR_CURRENT,
+//   UPDATE_CONTACT,
+//   FILTER_CONTACTS,
+//   CLEAR_CONTACTS,
+//   CLEAR_FILTER,
+//   CONTACT_ERROR,
+// } from '../types'
+
+// const ContactState = (props) => {
+//   const initialState = [
+//     {
+//       id: '1',
+//       name: 'a',
+//       email: 'a@gmail.com',
+//       phone: '123',
+//       type: 'personal',
+//     },
+//     {
+//       id: '2',
+//       name: 'b',
+//       email: 'b@gmail.com',
+//       phone: '123',
+//       type: 'personal',
+//     },
+//     {
+//       id: '3',
+//       name: 'c',
+//       email: 'c@gmail.com',
+//       phone: '123',
+//       type: 'personal',
+//     },
+//   ]
+// }
