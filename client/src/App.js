04f994562ec48d3b4522047acc9166560efecbe6
Isabelle-123 <isacas031@gmail.com>
2020-09-18 12:03:57 +0200

    added auth state

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import Home from './components/pages/Home'
 import About from './components/pages/About'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   )
 }
 
