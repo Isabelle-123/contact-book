4336e0048d89de87892160b86f0d78125a1c0ffe
Isabelle-123 <isacas031@gmail.com>
2020-09-24 11:58:58 +0200

    tried again

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -1,16 +1,24 @@
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
-  //   USER_LOADED,
-  //   AUTH_ERROR,
+
   //   LOGIN_SUCCESS,
   //   LOGIN_FAIL,
   //   LOGOUT,
-  //   CLEAR_ERRORS,
+  CLEAR_ERRORS,
+  USER_LOADED,
+  AUTH_ERROR,
 } from '../types'
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      }
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token)
       return {
@@ -20,6 +28,7 @@ export default (state, action) => {
         loading: false,
       }
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token')
       return {
         ...state,
@@ -28,6 +37,11 @@ export default (state, action) => {
         loading: false,
         user: null,
         error: action.payload,
+      }
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
       }
     default:
       return state
